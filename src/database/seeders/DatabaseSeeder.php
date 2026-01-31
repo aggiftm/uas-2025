d431bad70701bc8210f4244a6869a6a58eab858d
root <root@AggiFratama.>
2026-01-31 14:15:27 +0700

    new

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -5,13 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Kelas;
-use App\Models\JenjangPendidikan;
-use App\Models\Guru;
-use App\Models\MataPelajaran;
-use App\Models\Siswa;
-use App\Models\GuruMataPelajaran;
-use App\Models\GuruSiswa;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,13 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            KelasSeeder::class,
-            JenjangPendidikanSeeder::class,
-            GuruSeeder::class,
-            MataPelajaranSeeder::class,
-            SiswaSeeder::class,
-            GuruMataPelajaranSeeder::class,
-            GuruSiswaSeeder::class,
         ]);
     }
 }
